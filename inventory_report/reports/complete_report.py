4ee08eee9df6ab64b3c74c21acbecb456031aec4
alissonRH <alisson.honorato@live.com>
2023-02-01 16:05:33 -0300

    teste commit

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,22 @@
-class CompleteReport:
-    pass
+from collections import Counter
+from typing import List, Dict
+from inventory_report.reports.simple_report import SimpleReport
+
+
+class CompleteReport(SimpleReport):
+    @staticmethod
+    def generate(reports: List[Dict]):
+        simple_report = SimpleReport.generate(reports)
+
+        stocked_products = Counter(
+            [report["nome_da_empresa"] for report in reports]
+        )
+        stocked_products_str = ""
+        for company, stock in stocked_products.items():
+            stocked_products_str += f"- {company}: {stock}\n"
+
+        return (
+            f"{simple_report}\n"
+            "Produtos estocados por empresa:\n"
+            f"{stocked_products_str}"
+        )
--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,4 +1,4 @@
-"""   
+"""
 Entrada
 [
      {
@@ -26,15 +26,15 @@ class SimpleReport:
     def generate(reports: List[Dict]):
         fabrication_list = [
             convert_in_date(report["data_de_fabricacao"]) for report in reports
-            ]
-        oldest_fabrication = min(fabrication_list).strftime('%Y-%m-%d')
+        ]
+        oldest_fabrication = min(fabrication_list).strftime("%Y-%m-%d")
         validity_list = [
             convert_in_date(report["data_de_validade"]) for report in reports
-            ]
+        ]
         now = datetime.now()
         nearest_validity = min(
             validity for validity in validity_list if validity > now
-            ).strftime('%Y-%m-%d')
+        ).strftime("%Y-%m-%d")
         companies = [report["nome_da_empresa"] for report in reports]
         company_with_more_products = max(set(companies), key=companies.count)
 
@@ -42,4 +42,4 @@ class SimpleReport:
             f"Data de fabricação mais antiga: {oldest_fabrication}\n"
             f"Data de validade mais próxima: {nearest_validity}\n"
             f"Empresa com mais produtos: {company_with_more_products}"
-            )
+        )
