c6437d8dd03960a0251d5f641adb5446da5710ad
alissonRH <alisson.honorato@live.com>
2023-02-01 19:32:50 -0300

    testes

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,27 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    id = 1
+    nome_do_produto = "Suco de Laranja"
+    nome_da_empresa = "Suq"
+    data_de_fabricacao = "01/02/2023"
+    data_de_validade = "10/02/2023"
+    numero_de_serie = "123456789"
+    instrucoes_de_armazenamento = "Consumir em até 3 dias após aberto"
+    product_print = (
+        "O produto Suco de Laranja fabricado em 01/02/2023 por Suq "
+        "com validade até 10/02/2023 "
+        "precisa ser armazenado Consumir em até 3 dias após aberto."
+    )
+
+    product = Product(
+        id,
+        nome_do_produto,
+        nome_da_empresa,
+        data_de_fabricacao,
+        data_de_validade,
+        numero_de_serie,
+        instrucoes_de_armazenamento,
+    )
+    assert product.__repr__() == product_print
--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,20 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.importer.csv_importer import CsvImporter
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    colored_phrases = [
+        "\033[32mData de fabricação mais antiga:\033[0m",
+        "\033[32mData de validade mais próxima:\033[0m",
+        "\033[32mEmpresa com mais produtos:\033[0m",
+        "\033[36m2020-09-06\033[0m",
+        "\033[36m2023-09-17\033[0m",
+        "\033[31mTarget Corporation\033[0m",
+    ]
+    path = "inventory_report/data/inventory.csv"
+    reports = CsvImporter.import_data(path)
+    colored_report = ColoredReport(SimpleReport)
+    report_list = colored_report.generate(reports)
+    for phrase in colored_phrases:
+        assert phrase in report_list
